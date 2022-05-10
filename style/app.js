56f7fb0fe7c5e066d40961d042fd625ec75f9ac7
BravoDiego <101215860+BravoDiego@users.noreply.github.com>
2022-05-10 21:02:39 +0200

    Add files via upload

--- style/app.js
+++ style/app.js
@@ -1,35 +1,43 @@
-var back = document.getElementById('back');
-var next = document.getElementById('next');
-var un = document.getElementById('un');
-var deux = document.getElementById('deux');
+var btnOne = document.querySelector("#un");
+var btnTwo = document.querySelector("#deux");
+var chevronLeft = document.querySelector("#chevronleft");
+var chevronRigth = document.querySelector("#chevronrigth");
 
-var main = document.querySelector('main');
-var page2 = document.querySelector('#projects2');
+var pageOne = document.querySelector("main");
+var projectsOne = document.querySelector("#projects");
+var projectsTwo = document.querySelector("#projectstwo");
 
-var page = 1;
+var pageActuel = 1;
 
-back.addEventListener("click", PageOne);
-next.addEventListener("click", PageTwo);
-un.addEventListener("click", PageOne);
-deux.addEventListener("click", PageTwo);
+btnOne.addEventListener("click", function() {
+    pageActuel = 1;
+    Switch();
+});
+btnTwo.addEventListener("click", function() {
+    pageActuel = 2;
+    Switch();
+});
+chevronLeft.addEventListener("click", function() {
+    pageActuel = 1;
+    Switch();
+});
+chevronRigth.addEventListener("click", function() {
+    pageActuel = 2;
+    Switch();
+});
 
-function PageTwo() {
-    console.log(deux);
-    page = 2;
-    main.style.display = 'none';
-    page2.style.display = 'flex';
-    un.style.backgroundColor = '#F2F2F2';
-    un.style.color = '#2acd73';
-    deux.style.backgroundColor = '#2acd73';
-    deux.style.color = '#F2F2F2';    
-}
-
-function PageOne() {
-    page=1;
-    main.style.display = 'flex';
-    page2.style.display = 'none';
-    deux.style.backgroundColor = '#F2F2F2';
-    deux.style.color = '#2acd73';
-    un.style.backgroundColor = '#2acd73';
-    un.style.color = '#F2F2F2';    
+function Switch() {
+    if (pageActuel === 1) {
+        pageOne.style.display = 'grid';
+        projectsOne.style.display = 'grid';
+        projectsTwo.style.display = 'none';
+        btnOne.classList.add("active");
+        btnTwo.classList.remove("active");
+    } else {
+        pageOne.style.display = 'none';
+        projectsOne.style.display = 'none';
+        projectsTwo.style.display = 'grid';
+        btnTwo.classList.add("active");
+        btnOne.classList.remove("active");
+    }
 }
